23787821c0fa791fc73d3c7da42cb9e811e914d9
Ali Alaa <alialaatv@gmail.com>
2017-03-24 19:36:32 +0200

    Update Build

--- dist/bundle.js
+++ dist/bundle.js
@@ -496,16 +496,16 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
 //Helpers (from http://jaketrent.com/post/addremove-classes-raw-javascript/)
 function hasClass(el, className) {
-		if (el.classList) return el.classList.contains(className);else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+	if (el.classList) return el.classList.contains(className);else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
 }
 function addClass(el, className) {
-		if (el.classList) el.classList.add(className);else if (!hasClass(el, className)) el.className += " " + className;
+	if (el.classList) el.classList.add(className);else if (!hasClass(el, className)) el.className += " " + className;
 }
 function removeClass(el, className) {
-		if (el.classList) el.classList.remove(className);else if (hasClass(el, className)) {
-				var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-				el.className = el.className.replace(reg, ' ');
-		}
+	if (el.classList) el.classList.remove(className);else if (hasClass(el, className)) {
+		var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+		el.className = el.className.replace(reg, ' ');
+	}
 }
 
 /*
@@ -513,104 +513,124 @@ Helper function that takes the object returned from isTerminal() and adds a
 class to the board that will handle drawing the winning line's animation
 */
 function drawWinningLine(_ref) {
-		var direction = _ref.direction,
-		    row = _ref.row;
+	var direction = _ref.direction,
+	    row = _ref.row;
 
-		var board = document.getElementById("board");
-		board.className = '' + direction + row;
-		setTimeout(function () {
-				board.className += ' full';
-		}, 50);
+	var board = document.getElementById("board");
+	board.className = '' + direction + row;
+	setTimeout(function () {
+		board.className += ' full';
+	}, 50);
 }
 
 //Starts a new game with a certain depth and a starting_player of 1 if human is going to start
 function newGame() {
-		var depth = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : -1;
-		var starting_player = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+	var depth = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : -1;
+	var starting_player = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
 
-		//Instantiating a new player and an empty board
-		var p = new _Player2.default(parseInt(depth));
-		var b = new _Board2.default(['', '', '', '', '', '', '', '', '']);
+	//Instantiating a new player and an empty board
+	var p = new _Player2.default(parseInt(depth));
+	var b = new _Board2.default(['', '', '', '', '', '', '', '', '']);
 
-		//Clearing all #Board classes and populating cells HTML
-		var board = document.getElementById("board");
-		board.className = '';
-		board.innerHTML = '<div class="cell-0"></div><div class="cell-1"></div><div class="cell-2"></div><div class="cell-3"></div><div class="cell-4"></div><div class="cell-5"></div><div class="cell-6"></div><div class="cell-7"></div><div class="cell-8"></div>';
+	//Clearing all #Board classes and populating cells HTML
+	var board = document.getElementById("board");
+	board.className = '';
+	board.innerHTML = '<div class="cell-0"></div><div class="cell-1"></div><div class="cell-2"></div><div class="cell-3"></div><div class="cell-4"></div><div class="cell-5"></div><div class="cell-6"></div><div class="cell-7"></div><div class="cell-8"></div>';
 
-		//Storing HTML cells in an array
-		var html_cells = [].concat(_toConsumableArray(board.children));
+	//Clearing all celebrations classes
+	removeClass(document.getElementById("charachters"), 'celebrate_human');
+	removeClass(document.getElementById("charachters"), 'celebrate_robot');
 
-		//Initializing some variables for internal use
-		var starting = parseInt(starting_player),
-		    maximizing = starting,
-		    player_turn = starting;
+	//Storing HTML cells in an array
+	var html_cells = [].concat(_toConsumableArray(board.children));
 
-		//If computer is going to start, choose a random cell as long as it is the center or a corner
-		if (!starting) {
-				var center_and_corners = [0, 2, 4, 6, 8];
-				var first_choice = center_and_corners[Math.floor(Math.random() * center_and_corners.length)];
+	//Initializing some variables for internal use
+	var starting = parseInt(starting_player),
+	    maximizing = starting,
+	    player_turn = starting;
+
+	//If computer is going to start, choose a random cell as long as it is the center or a corner
+	if (!starting) {
+		var center_and_corners = [0, 2, 4, 6, 8];
+		var first_choice = center_and_corners[Math.floor(Math.random() * center_and_corners.length)];
+		var symbol = !maximizing ? 'x' : 'o';
+		b.insert(symbol, first_choice);
+		addClass(html_cells[first_choice], symbol);
+		player_turn = 1; //Switch turns
+	}
+
+	//Adding Click event listener for each cell
+	b.state.forEach(function (cell, index) {
+		html_cells[index].addEventListener('click', function () {
+			//If cell is already occupied or the board is in a terminal state or it's not humans turn, return false
+			if (hasClass(html_cells[index], 'x') || hasClass(html_cells[index], 'o') || b.isTerminal() || !player_turn) return false;
+
+			var symbol = maximizing ? 'x' : 'o'; //Maximizing player is always 'x'
+
+			//Update the Board class instance as well as the Board UI
+			b.insert(symbol, index);
+			addClass(html_cells[index], symbol);
+
+			//If it's a terminal move and it's not a draw, then human won
+			if (b.isTerminal()) {
+				var _b$isTerminal = b.isTerminal(),
+				    winner = _b$isTerminal.winner;
+
+				if (winner !== 'draw') addClass(document.getElementById("charachters"), 'celebrate_human');
+				drawWinningLine(b.isTerminal());
+			}
+			player_turn = 0; //Switch turns
+
+			//Get computer's best move and update the UI
+			p.getBestMove(b, !maximizing, function (best) {
 				var symbol = !maximizing ? 'x' : 'o';
-				b.insert(symbol, first_choice);
-				addClass(html_cells[first_choice], symbol);
+				b.insert(symbol, best);
+				addClass(html_cells[best], symbol);
+				if (b.isTerminal()) {
+					var _b$isTerminal2 = b.isTerminal(),
+					    _winner = _b$isTerminal2.winner;
+
+					if (_winner !== 'draw') addClass(document.getElementById("charachters"), 'celebrate_robot');
+					drawWinningLine(b.isTerminal());
+				}
 				player_turn = 1; //Switch turns
-		}
-
-		//Adding Click event listener for each cell
-		b.state.forEach(function (cell, index) {
-				html_cells[index].addEventListener('click', function () {
-						//If cell is already occupied or the board is in a terminal state or it's not humans turn, return false
-						if (hasClass(html_cells[index], 'x') || hasClass(html_cells[index], 'o') || b.isTerminal() || !player_turn) return false;
-
-						var symbol = maximizing ? 'x' : 'o'; //Maximizing player is always 'x'
-
-						//Update the Board class instance as well as the Board UI
-						b.insert(symbol, index);
-						addClass(html_cells[index], symbol);
-
-						//If it's a terminal move and it's not a draw, then human won
-						if (b.isTerminal()) {
-								var _b$isTerminal = b.isTerminal(),
-								    winner = _b$isTerminal.winner;
-
-								drawWinningLine(b.isTerminal());
-						}
-						player_turn = 0; //Switch turns
-
-						//Get computer's best move and update the UI
-						p.getBestMove(b, !maximizing, function (best) {
-								var symbol = !maximizing ? 'x' : 'o';
-								b.insert(symbol, best);
-								addClass(html_cells[best], symbol);
-								if (b.isTerminal()) {
-										var _b$isTerminal2 = b.isTerminal(),
-										    _winner = _b$isTerminal2.winner;
-
-										drawWinningLine(b.isTerminal());
-								}
-								player_turn = 1; //Switch turns
-						});
-				}, false);
-				if (cell) addClass(html_cells[index], cell);
-		});
+			});
+		}, false);
+		if (cell) addClass(html_cells[index], cell);
+	});
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
-		//Start a new game when page loads with default values
-		var depth = -1;
-		var starting_player = 1;
-		newGame(depth, starting_player);
+	//Start a new game when page loads with default values
+	var depth = -1;
+	var starting_player = 1;
+	newGame(depth, starting_player);
 
-		document.getElementById("newgame").addEventListener('click', function () {
-
-				var starting = document.getElementById("starting");
-				var starting = starting.options[starting.selectedIndex].value;
-
-				var depth = document.getElementById("depth");
-				var depth = depth.options[depth.selectedIndex].value;
-
-				newGame(depth, starting);
+	//Events handlers for depth, starting player options
+	document.getElementById("depth").addEventListener("click", function (event) {
+		if (event.target.tagName !== "LI" || hasClass(event.target, 'active')) return;
+		var depth_choices = [].concat(_toConsumableArray(document.getElementById("depth").children[0].children));
+		depth_choices.forEach(function (choice) {
+			removeClass(choice, 'active');
 		});
+		addClass(event.target, 'active');
+		depth = event.target.dataset.value;
+	}, false);
+
+	document.getElementById("starting_player").addEventListener("click", function (event) {
+		if (event.target.tagName !== "LI" || hasClass(event.target, 'active')) return;
+		var starting_player_choices = [].concat(_toConsumableArray(document.getElementById("starting_player").children[0].children));
+		starting_player_choices.forEach(function (choice) {
+			removeClass(choice, 'active');
+		});
+		addClass(event.target, 'active');
+		starting_player = event.target.dataset.value;
+	}, false);
+
+	document.getElementById("newgame").addEventListener('click', function () {
+		newGame(depth, starting_player);
+	});
 });
 
 /***/ })

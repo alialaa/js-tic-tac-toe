17b41c1eec3328a48e08b884bbc1c4a69c0971a5
Ali Alaa <alialaatv@gmail.com>
2017-03-24 19:43:28 +0200

    Updated public path for gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   	output: {
 	    path: path.join(__dirname, 'dist'),
 	    filename: '[name].js',
-	    publicPath: 'dist/'
+	    publicPath: 'js-tic-tac-toe/dist/'
   	},
   	devtool: "source-map",
 	module: {
